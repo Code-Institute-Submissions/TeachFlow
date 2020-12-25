0db8d59d5aec5a4795f4a8b98ac4cc015142543d
emmahartedev <emmaharte33@outlook.com>
2020-12-25 21:00:59 +0000

    Add explore destination to index.html

---
+++ maps.js
@@ -0,0 +1,8 @@
+const choosedestbutton = document.getElementsByClassName("tf-button");
+for (let i = 0; i < tf-button.length; i++) {
+    tf-button[i].addEventListener("click", displaydestinfo);
+}
+
+function displaydestinfo(dest) {
+    const
+}
