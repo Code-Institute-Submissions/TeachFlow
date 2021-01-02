748a41321ac32c8cf1aacf9558a42a18f212e911
emmahartedev <emmaharte33@outlook.com>
2021-01-02 14:02:38 +0000

    Add markers to maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,3 +1,4 @@
+//------------------ function to display the map and info contents ------------ //
 /* inspiration taken from https://www.w3schools.com/howto/howto_js_full_page_tabs.asp*/
 function openPage(pageName, elmnt) {
                 // once page loads, hide all elements with class="countrymap" */
@@ -5,9 +6,9 @@ function openPage(pageName, elmnt) {
                 let countrymap;
                 let tfbutton; 
 
-                // define the country map
-                countrymap = document.getElementsByClassName("countrymap");
-                for (i = 0; i < countrymap.length; i++) {
+                // define the country map, set all to display none (no map or contents)
+                countrymap = document.getElementsByClassName("countrymap"); // returns a collection of all elements in the document with the specified class name
+                for (i = 0; i < countrymap.length; i++) {  // countrymap.length == 4
                     countrymap[i].style.display = "none"; 
                 }
 
@@ -17,23 +18,24 @@ function openPage(pageName, elmnt) {
                     tfbutton[i].style.backgroundColor = ""; 
                 }
 
-                // Set the content to block, page name given as arg to func
+                // Set the content to display block, page name given as arg to func
                 document.getElementById(pageName).style.display = "block";
 
-                // Add the specific color to the active button 
+                // Add the specific color to the 'this' active button 
                 elmnt.style.backgroundColor = "#034cae";
                 setTimeout(function(){
                     mapJapan.invalidateSize(); //Checks if the map container size changed and updates the map if so
                     mapChina.invalidateSize();
                     mapThailand.invalidateSize();
                 }, 0);
+
             } 
 
             // Get the element with id="defaultOpen" and click on it
             document.getElementById("defaultOpen").click();
 
 
-            //Create maps 
+//------------------------------------ Create maps 
 
             //sk
             let LatSouthKorea = 35.9078;
@@ -50,9 +52,13 @@ function openPage(pageName, elmnt) {
                 attribution: '<a href="https://www.google.com/maps">Google Maps</a>',
                 maxZoom: 20,
                 subdomains:['mt0','mt1','mt2','mt3']
-            });
-            googleStreetsSouthKorea.addTo(mapSouthKorea);
-
+                // add markers
+            }).addTo(mapSouthKorea);
+            
+            // add some markers
+            let seoul = L.marker([37.9078, 126.9780]).addTo(mapSouthKorea).bindPopup("Seoul");;
+            let busan = L.marker([35.1796, 129.0756]).addTo(mapSouthKorea).bindPopup("Busan");;
+            let incheon = L.marker([37.4563, 126.7052]).addTo(mapSouthKorea).bindPopup("Incheon");;
 
             // japan
             let LatJapan = 36.20;
@@ -69,9 +75,11 @@ function openPage(pageName, elmnt) {
                 attribution: '<a href="https://www.google.com/maps">Google Maps</a>',
                 maxZoom: 20,
                 subdomains:['mt0','mt1','mt2','mt3']
-            });
-            googleStreetsJapan.addTo(mapJapan);
+            }).addTo(mapJapan);
 
+            let osaka = L.marker([34.6937, 135.5023]).addTo(mapJapan).bindPopup("Osaka");
+            let tokyo = L.marker([35.6762, 139.6503]).addTo(mapJapan).bindPopup("Tokyo");;
+            let sapporo = L.marker([43.0618, 141.3545]).addTo(mapJapan).bindPopup("Sapporo");;
 
             // china
             let LatChina = 35.8617;
@@ -88,8 +96,11 @@ function openPage(pageName, elmnt) {
                 attribution: '<a href="https://www.google.com/maps">Google Maps</a>',
                 maxZoom: 20,
                 subdomains:['mt0','mt1','mt2','mt3']
-            });
-            googleStreetsChina.addTo(mapChina);
+            }).addTo(mapChina);
+
+            let beijing = L.marker([39.9042, 116.4074]).addTo(mapChina).bindPopup("Beijing");
+            let guangzhou = L.marker([23.1291, 113.2644]).addTo(mapChina).bindPopup("Guangzhou");
+            let shenzhen = L.marker([22.5431, 114.0579]).addTo(mapChina).bindPopup("Shenzhen");
 
             // Thailand
             let LatThailand = 11.973342;
@@ -106,14 +117,18 @@ function openPage(pageName, elmnt) {
                 attribution: '<a href="https://www.google.com/maps">Google Maps</a>',
                 maxZoom: 20,
                 subdomains:['mt0','mt1','mt2','mt3']
-            });
-            googleStreetsThailand.addTo(mapThailand);
+            }).addTo(mapThailand);
 
+            let chiangmai = L.marker([18.7883, 98.9853]).addTo(mapThailand).bindPopup("Chiang mai");
+            let bangkok = L.marker([13.7563, 100.5018]).addTo(mapThailand).bindPopup("Bangkok");
+            let phuket = L.marker([7.8804, 98.3923]).addTo(mapThailand).bindPopup("Phuket");
+
+            
             // set info
 
             // sk info
 
-            document.getElementById("infoSouthKorea").innerHTML = 
+           /* document.getElementById("infoSouthKorea").innerHTML = 
             `<h4 class = "heading">Information:</h4>
              <p>South Korea is filled with beautiful beaches, thriving cities, ancient temples, remarkable natural scenery and most importantly, friendly people with ancient history. </p>
              <br>
@@ -124,9 +139,10 @@ function openPage(pageName, elmnt) {
              <span>Requirements:</span> Bachelor's degree & E-2 visa
              <br> <br>
              <span>Benefits:</span> Paid airfare, housing & end of contract bonus`;
+            */ 
 
             // Japan info
-            document.getElementById("infoJapan").innerHTML = 
+           /* document.getElementById("infoJapan").innerHTML = 
             `<h4 class = "heading">Information:</h4>
              <p>Japan is a string of islands, on the eastern edge of Asia. There are four main islands: Hokkaido, Honshu, Shikoku, and Kyushu. There are also nearly 4,000 smaller islands! </p>
              <br>
@@ -137,9 +153,9 @@ function openPage(pageName, elmnt) {
              <span>Requirements:</span> Bachelor's degree & completed visa form
              <br> <br>
              <span>Benefits:</span> Paid airfare, housing/housing allowance`;
-
+*/
             // china info
-            document.getElementById("infoChina").innerHTML = 
+           /* document.getElementById("infoChina").innerHTML = 
             `<h4 class = "heading">Information:</h4>
              <p>China is the largest of all Asian countries and has the largest population of any country in the world. Occupying nearly the entire East Asian landmass, it covers approximately one-fourteenth of the land area of Earth.</p>
              <br>
@@ -150,7 +166,8 @@ function openPage(pageName, elmnt) {
              <span>Requirements:</span> Bachelor's degree & Z visa
              <br> <br>
              <span>Benefits:</span> Low cost of Living`;
-
+*/
+/*
             // Thailand info
             document.getElementById("infoThailand").innerHTML = 
             `<h4 class = "heading">Information:</h4>
@@ -163,3 +180,4 @@ function openPage(pageName, elmnt) {
              <span>Requirements:</span> Bachelor's degree & B visa
              <br> <br>
              <span>Benefits:</span> Low cost of Living`;
+             */
