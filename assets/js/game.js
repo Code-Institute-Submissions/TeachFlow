829ce11c843a64cba891fb4d3f8a15c1724a03ac
emmahartedev <emmaharte33@outlook.com>
2021-01-03 18:32:26 +0000

    Ammend code in game.js and add comments

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,4 @@
-// function to add sound to flashcards //
+//------------------------------------ function to add sound to flashcards ------------------------------------//
 
 function playSound(animal) {
     let sound = document.querySelector(`[data-sound='${animal}']`);
@@ -6,9 +6,9 @@ function playSound(animal) {
 }
 
 
-// memory game
+//------------------------------------ memory game ------------------------------------ //
 
-// instructions button
+// ------------------------------------ instructions button ------------------------------------
 let instructbtn = document.querySelector('.instruct-btn').addEventListener("click", toggleDisplay); // get the instruct-btn and add an event listener, toggler on click
     function toggleDisplay() {
         let desc = document.querySelector(".showDesc"); //desc is the showDesc class
@@ -19,19 +19,20 @@ let instructbtn = document.querySelector('.instruct-btn').addEventListener("clic
         }
     }
 
- //------------------------------------ declare variables & arrrays ----------------------- //
+ //------------------------------------ declare variables & arrrays for game ----------------------- //
 
-let orderOfTiles = []; // An array to store the actual order of tiles
-let orderByPlayer = []; // Stores the order of tiles played by player
-let round = 0; //create a level which will increment at each new round
+let orderOfTiles = []; // the actual order of tiles in each round
+let orderByPlayer = []; // the order of tiles played by player
+let round = 0; //the level, incremented at each new round
 
 let beginGameButton = document.querySelector('.js-begin-game'); // game button
 let gameText = document.querySelector('.js-text'); // info replaced by button
 
-let heading = document.querySelector('.js-title');// define the heading and tile container
-let gameTiles = document.querySelector('.js-gameTiles');
+let heading = document.querySelector('.js-title');// the heading
+let gameTiles = document.querySelector('.js-gameTiles'); // tile container
 
-function resetGame(text) {
+// end of function //
+function gameOver(text) {
     swal(text); /*credit https://sweetalert.js.org/*/
     orderOfTiles = [];
     orderByPlayer = [];
@@ -42,7 +43,7 @@ function resetGame(text) {
     gameTiles.classList.add('unclickable');
 }
 
-// players turn
+// players turn, allow them to click tiles and add some text 
 function playersTurn(round) {
     gameTiles.classList.remove('unclickable'); // allows the tiles to be clicked
     gameText.innerHTML = `Your Go! <br> ${round} Tap${round > 1 ? 's' : ''}`;
@@ -61,17 +62,17 @@ function tilePressed(animal) {
     }, 200);
 }
 
-
-function playRound(nextSequence) { //next seq contains an array of all round values stored
-    nextSequence.forEach((animal, index) => {
+// iterate through the tiles in each round, to play sound and change pic for every tile.
+function itertateThrough(storedRoundTiles) { 
+    storedRoundTiles.forEach((animal, index) => { //iterate through all tiles (index = index in array)
         setTimeout(() => {
             tilePressed(animal); // initiate the tilePressed function
-        }, (index + 1) * 850); // tested to suit audio & avoid overlapping
+        }, (index + 1) * 850); // allow time between tiles
     });
 }
 
-
-function nextStep() {
+// generate a random new tile 
+function newTileinRound() {
     let tiles = ['chicken', 'duck', 'pig', 'cow'];
     let random = tiles[Math.floor(Math.random() * tiles.length)]; // multiply the value from math.random() with the length of the tiles, which gives a range from 0-3
 
@@ -79,45 +80,46 @@ function nextStep() {
 }
 
 
+// start the round - set text, iterate through the tiles, add one, switch to players turn.
 // runs after start game is pressed & when a round has been completed correctly
-function nextRound() {
+function startRound() {
     round +=1; // increment round
     gameTiles.classList.add('unclickable');
     gameText.textContent = "Listen and Watch";
     heading.textContent = `Round ${round}, Remaining: ${20-round} `;
-    let nextSequence = [...orderOfTiles]; // store the array of tiles in round ["cow", "chicken"]
-    nextSequence.push(nextStep()); // adds  nextStep ["cow"] to the end of nextSequence{} with all other values from other rounds.
-    playRound(nextSequence);
+    let storedRoundTiles = [...orderOfTiles]; // store the array of tiles eg ["cow", "chicken"]
+    storedRoundTiles.push(newTileinRound()); // adds newTileinRound ["cow"] to the end of storedRoundTiles{} with all other values from other rounds.
+    itertateThrough(storedRoundTiles); // trigger to play sound & change pic
 
-    orderOfTiles = [... nextSequence];
+    orderOfTiles = [... storedRoundTiles]; //reverse it back
     setTimeout(() => {
-        playersTurn(round); 
-    }, round * 600 + 1000); // add a delay and calc when the computer is done with the button tap sequence
+        playersTurn(round); // initiate the playersturn
+    }, round * 700 + 1000); // add a delay to stop text changing immediately
 }
 
-
+// action taken each time a tile is clicked
 function responseToClick(tile) {
-    let index = orderByPlayer.push(tile) -1; // pushes the tile value to the orderByPlayer array
-    let sound = document.querySelector(`[data-sound='${tile}']`); //plays the corresponding sound
-    sound.play();
+    let sound = document.querySelector(`[data-sound='${tile}']`); 
+    sound.play(); //plays the corresponding sound
+    let indexofTile = orderByPlayer.push(tile) -1; //stores the index of the tile in the orderByPlayer eg ['chicken = 0, 'pig = 1']
 
-    let remainingTaps = orderOfTiles.length - orderByPlayer.length; //calc remaining steps
-
-    if(orderByPlayer[index] !== orderOfTiles[index]) {
-        resetGame('Oh no! you made an incorrect move, start again');
-        return; //if the value retrieved by index does not match in index and sequence
+    let remainingTaps = orderOfTiles.length - orderByPlayer.length; //calc remaining moves to make
+    // end game
+    if(orderByPlayer[indexofTile] !== orderOfTiles[indexofTile]) { // orderByPlayer[indexofTile]= name of tile(animal) pressed by player, orderOfTiles[indexofTile]= name of correct tile(animal)
+        gameOver(`Whoops! You made an incorrect move, start again.`);
+        return;
     }
-
+    
     if(orderByPlayer.length === orderOfTiles.length) { //if equal, round is over
-        
+        // game complete - first check this
         if (orderByPlayer.length === 15) {
-            resetGame('Superstar! you completed all rounds!');
+            gameOver('Superstar! you completed all rounds!');
             return //if 15 levels complete then finish game
-        }
+        } 
         orderByPlayer = [];  
         gameText.textContent = "Correct! Keep going!"; 
         setTimeout(() => {
-            nextRound();
+            startRound();
         }, 1000); //delay to allow users to see the success message
         return;
     }
@@ -130,14 +132,14 @@ function startGame() {
     beginGameButton.classList.add('hidden');
     gameText.classList.remove('hidden');
     gameText.textContent = 'Listen and Watch'; //add the text
-    nextRound(); // instiate the first round
+    startRound(); // instiate the first round
 }
 
-// event listeners
+// ------------------------------------ event listeners ------------------------------------//
  
 beginGameButton.addEventListener('click', startGame); //start game function when the button is clicked
 
-gameTiles.addEventListener('click', event => { // decide if the players round is a success & they can move on or not
+gameTiles.addEventListener('click', function (event) { // check the action when a game tile is pressed 
     let {tile} = event.target.dataset; //stores the data-tile value 
     if(tile) responseToClick(tile); //if value is not an empty string execute responseToClick
 });
