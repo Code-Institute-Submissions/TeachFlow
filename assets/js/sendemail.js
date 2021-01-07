950811363db9b2df843dd02b982d8e9c17f97120
emmahartedev <emmaharte33@outlook.com>
2021-01-07 21:42:23 +0000

    Add hide form functionality to contact form in contact.html

--- assets/js/sendemail.js
+++ assets/js/sendemail.js
@@ -1,12 +1,16 @@
 function sendMail(contactForm) {
     emailjs.send("google", "teachflow", {
-        "from_name": contactForm.fname.value,
+        "from_fname": contactForm.firstname.value,
+        "from_lname": contactForm.lastname.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.message.value
+        "message": contactForm.yourmessage.value,
+        "subscribe":contactForm.subscribe.value
     })
     .then(
-        function(response) {
-            console.log("SUCCESS", response);
+        function() {
+            console.log("SUCCESS");
+            let form = document.getElementById('jswrapper');
+            form.innerHTML= `<h3 class = "special-text-main">Your message has seen successfully sent!</h3`;
         },
         function(error) {
             console.log("FAILED", error);
