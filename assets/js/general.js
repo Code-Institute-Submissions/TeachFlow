a49599c94cb84be0920a1563126ee6be0ce9fbb6
emmahartedev <emmaharte33@outlook.com>
2021-01-10 19:18:08 +0000

    Add smart scrolling to Nav bar

--- assets/js/general.js
+++ assets/js/general.js
@@ -1,6 +1,41 @@
 
 //general page script //
 
+// add a scrolling effect, in-place of jumping page location 
+// credit: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_eff_animate_smoothscroll
+$(document).ready(function () {
+    // add to all <a></a> links
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      // Store hash
+      let hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
+
+// hide nav bar on scrolling
+// CREDIT: https://bootstrap-menu.com/detail-smart-hide.html
+if ($('.fixed-navbar').length > 0) {
+    let last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top){
+            $('.fixed-navbar').removeClass('scroll-down').addClass('scroll-up');
+        } else {
+            $('.fixed-navbar').removeClass('scroll-up').addClass('scroll-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
 
 // add active class to home by default
 $(document).ready(function () {
