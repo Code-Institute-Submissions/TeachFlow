b399ae4754784b090903a5883ac6fb6656f43181
emmahartedev <emmaharte33@outlook.com>
2021-01-17 22:24:57 +0000

    Ammend fixed navbar scroll down bug

--- assets/js/general.js
+++ assets/js/general.js
@@ -15,22 +15,66 @@ $(".nav-link").on("click", function() {
     $(this).addClass("active");
 }); 
 
-
 // Hide nav bar on scrolling
 // CREDIT: https://bootstrap-menu.com/detail-smart-hide.html
-if ($('.fixed-navbar').length > 0) {
+/*if ($('.fixed-navbar').length > 0) {
     let last_scroll_top = 0;
     $(window).on('scroll', function() {
-        scroll_top = $(this).scrollTop();
-        if(scroll_top < last_scroll_top){
-            $('.fixed-navbar').removeClass('scroll-down').addClass('scroll-up');
+        scroll_top = $(this).scrollTop(); //set scrolltop value as dist from top to first element
+        if(scroll_top < last_scroll_top)
+        {
+            $('.fixed-navbar').removeClass('scroll-down').addClass('scroll-up'); //add the navbar
         } else {
-            $('.fixed-navbar').removeClass('scroll-up').addClass('scroll-down');
+            
+            $('.fixed-navbar').removeClass('scroll-up').addClass('scroll-down'); //hide nav bar
         }
         last_scroll_top = scroll_top;
     });
-} 
+} */
+ /* hideTimer = setTimeout(() => {
+    document.getElementById("navbar").style.top = "0";
+  }, 500); // delayed by 0.5 second
+}*/
 
+// Hide Navbar on scroll down
+//https://codepen.io/fbmiranda/pen/edqgxm
+var didScroll;
+var lastScrollTop = 0;
+var delta = 15;
+var navbarHeight = $('nav').outerHeight();
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
+    
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('scroll-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('scroll-up');
+        }
+    }
+    
+    lastScrollTop = st;
+}
 // close the menu after each click (needed as some sections are on same page)
 window.onclick = function () {
   let nav = document.getElementById("teachflownav");
